f639ad592b7e52f24ae9d897c41601890725ecf6
Jeong seulho <wjdtmfgh@gmail.com>
2024-02-22 20:17:39 +0900

    Time: 59 ms (21.46%), Space: 49.3 MB (10.05%) - LeetHub

---
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -0,0 +1,13 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    let cnt = 0;
+    args.forEach(() => cnt++)
+    return cnt;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
