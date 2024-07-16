5b39f4a4307a8362ed0aed9bf371884e1bca196f
Jeong seulho <wjdtmfgh@gmail.com>
2024-07-16 10:20:49 +0900

    Time: 113 ms (27.84%), Space: 54.6 MB (90.2%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.js
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.js
@@ -5,8 +5,7 @@
  */
 var findDifference = function(nums1, nums2) {
     const duples = nums1.filter((num1) => nums2.includes(num1));
-    console.log(duples)
-    const ans1 = [...new Set(nums1.filter((num1) => !duples.includes(num1)))]
-    const ans2 = [...new Set(nums2.filter((num2) => !duples.includes(num2)))]
+    const ans1 = [...new Set(nums1.filter((num1) => !duples.includes(num1)))];
+    const ans2 = [...new Set(nums2.filter((num2) => !duples.includes(num2)))];
     return [ans1, ans2];
 };
