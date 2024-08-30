43c230d61f7b0276e8c5075ce690df34d3bc39f4
Jeong seulho <wjdtmfgh@gmail.com>
2024-08-30 16:42:47 +0900

    Time: 54 ms (41.53%), Space: 49.3 MB (10.91%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -7,14 +7,12 @@ var findPeakElement = function(nums) {
     while(low <= high) {
         const mid = Math.floor((low + high) / 2);
         
-        if (mid > 0 && nums[mid] < nums[mid - 1]) {
-            high = mid - 1; // 피크는 왼쪽에 있음
-        } else if (mid < nums.length - 1 && nums[mid] < nums[mid + 1]) {
+        if (mid < nums.length - 1 && nums[mid] < nums[mid + 1]) {
             low = mid + 1; // 피크는 오른쪽에 있음
         } else {
-            return mid; // 피크 발견
+            high = mid - 1; // 피크는 왼쪽에 있음
         }
     }
 
-    return -1; // 이론적으로는 실행되지 않음 (입력 배열에는 항상 피크가 존재함)
+    return low; // 피크 요소를 가리킴
 };
