a63f31de718c5a4b69e213e76e01ab775e8fa23c
Jeong seulho <wjdtmfgh@gmail.com>
2024-09-23 15:49:37 +0900

    Time: 69 ms (12.59%), Space: 51.1 MB (94.7%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,17 +11,17 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    if (!root) return 0;
+    let ans = 0;
+    if(!root) return 0;
+    
+    const dfs = (node, depth) => {
+        if(!node) return;
+        ans = Math.max(depth, ans);
 
-    const needVisit = [[root, 1]];
-    let maxDepth = 0;
-
-    while (needVisit.length) {
-        const [node, depth] = needVisit.pop();
-        maxDepth = Math.max(maxDepth, depth);
-        if (node.left) needVisit.push([node.left, depth + 1]);
-        if (node.right) needVisit.push([node.right, depth + 1]);
+        dfs(node.left, depth + 1);
+        dfs(node.right, depth + 1);
     }
 
-    return maxDepth;
+    dfs(root, 1);
+    return ans;
 };
