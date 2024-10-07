51418bc14fce715b83a64403874fa83e7bdea2be
Jeong seulho <wjdtmfgh@gmail.com>
2024-10-07 10:43:37 +0900

    Time: 65 ms (72.25%), Space: 52.4 MB (56.67%) - LeetHub

---
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root) {
+    let prev = null;
+    const revPreOrder = (node) => {
+        if(node.right) revPreOrder(node.right);
+        if(node.left) revPreOrder(node.left);
+        node.left = null;
+        node.right = prev;
+        prev = node;
+    }
+    if(root) revPreOrder(root);
+};
