b000c3bde05483e8b2e59a69c3d026dd216c3bc0
Jeong seulho <wjdtmfgh@gmail.com>
2024-10-21 12:31:46 +0900

    Time: 0 ms (100%), Space: 49 MB (59.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -4,14 +4,12 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    const dp = Array.from({ length : m }, () => Array(n).fill(0));
-
-    for(let i = 0; i < m; i++) {
-        for(let j = 0; j < n; j++) {
-            if(i === 0 || j === 0) dp[i][j] = 1;
-            else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    const dp = Array.from({ length : m }, () => Array(n).fill(1));
+    for(let i = 1; i < m; i++) {
+        for(let j = 1; j < n; j++) {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
         }
     }
-
+    
     return dp[m - 1][n - 1];
 };
