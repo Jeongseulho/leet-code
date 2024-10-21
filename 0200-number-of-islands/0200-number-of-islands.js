7837b0c99b425530ebf088c11d4a49c6bf313247
Jeong seulho <wjdtmfgh@gmail.com>
2024-10-21 15:53:50 +0900

    Time: 81 ms (44.12%), Space: 55.3 MB (50.29%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -4,12 +4,14 @@
  */
 var numIslands = function(grid) {
     const m = grid.length;
-    const n = grid[0].length
+    const n = grid[0].length;
+
     const di = [-1, 1, 0, 0];
     const dj = [0, 0, -1, 1];
 
     const dfs = (si, sj) => {
         const needVisit = [[si, sj]];
+        
         while(needVisit.length) {
             const [ci, cj] = needVisit.pop();
             for(let k = 0; k < 4; k++) {
@@ -22,17 +24,17 @@ var numIslands = function(grid) {
             }
         }
     }
-    
+
     let cnt = 0;
 
     for(let si = 0; si < m; si++) {
         for(let sj = 0; sj < n; sj++) {
             if(grid[si][sj] === '1') {
-                dfs(si, sj);
+                dfs(si, sj)
                 cnt += 1;
             }
         }
     }
-    
+
     return cnt;
 };
