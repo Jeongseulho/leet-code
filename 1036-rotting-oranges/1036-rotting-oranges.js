49d66d2e62d89e5ab051c45e797906f211705647
Jeong seulho <wjdtmfgh@gmail.com>
2024-10-22 10:24:06 +0900

    Time: 13 ms (97.74%), Space: 53.1 MB (53.48%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.js
+++ 1036-rotting-oranges/1036-rotting-oranges.js
@@ -3,49 +3,41 @@
  * @return {number}
  */
 var orangesRotting = function(grid) {
-    let maxTime = 0;
     const m = grid.length;
     const n = grid[0].length;
-    
+    const di = [-1, 1, 0, 0];
+    const dj = [0, 0, -1, 1];
+    let maxTime = 0;
+
     const bfs = (starts) => {
         const needVisit = [...starts];
-        const di = [0, 0, -1, 1];
-        const dj = [-1, 1, 0, 0];
         while(needVisit.length) {
             const [ci, cj, time] = needVisit.shift();
             for(let k = 0; k < 4; k++) {
-                const ni = ci + di[k];
-                const nj = cj + dj[k];
+                const ni = di[k] + ci;
+                const nj = dj[k] + cj;
                 if(ni > -1 && ni < m && nj > -1 && nj < n && grid[ni][nj] === 1) {
                     needVisit.push([ni, nj, time + 1]);
-                    maxTime = Math.max(maxTime, time + 1);
                     grid[ni][nj] = 2;
+                    maxTime = time + 1;
                 }
             }
         }
     }
 
-    const searchStart = () => {
-        const starts = [];
-        for(let i = 0; i < m; i++) {
-            for(let j = 0; j < n; j++) {
-                if(grid[i][j] === 2) starts.push([i, j, 0]);
-            }
+    const starts = [];
+    for(let i = 0; i < m; i++) {
+        for(let j = 0; j < n; j++) {
+            if(grid[i][j] === 2) starts.push([i, j, 0]);
         }
-        return starts;
+    }
+    bfs(starts)
+
+    for(let i = 0; i < m; i++) {
+        for(let j = 0; j < n; j++) {
+            if(grid[i][j] === 1) return -1;
+        }
     }
 
-    const isSomeFresh = () => {
-        for(let i = 0; i < m; i++) {
-            for(let j = 0; j < n; j++) {
-                if(grid[i][j] === 1) return true;
-            }
-        }
-        return false;
-    }
-
-    const starts = searchStart();
-    if(starts.length) bfs(starts);
-    
-    return isSomeFresh() ? -1 : maxTime;
+    return maxTime;
 };
