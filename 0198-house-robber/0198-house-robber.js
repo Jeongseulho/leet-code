7c45aa5e8274ea8f7ed4572b0cf32959c1c15e69
Jeong seulho <wjdtmfgh@gmail.com>
2024-10-23 09:19:44 +0900

    Time: 0 ms (100%), Space: 48.5 MB (79.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -3,16 +3,13 @@
  * @return {number}
  */
 var rob = function(nums) {
-    if(nums.length === 0) return 0;
     if(nums.length === 1) return nums[0];
-    
-    const dp = Array(nums.length).fill(0);
-    dp[0] = nums[0];
-    dp[1] = Math.max(nums[1], nums[0]);
+    if(nums.length === 2) return Math.max(...nums);
 
-    for(let i = 2; i < dp.length; i++) {
-        dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+    nums[1] = Math.max(nums[0], nums[1]);
+    for(let i = 2; i < nums.length; i++) {
+        nums[i] = Math.max(nums[i - 2] + nums[i], nums[i - 1]);
     }
 
-    return dp.at(-1)
+    return nums.at(-1);
 };
