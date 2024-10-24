c1a73740d4e3db1703d7ae500e4c5632dcbf5e41
Jeong seulho <wjdtmfgh@gmail.com>
2024-10-24 14:14:13 +0900

    Time: 73 ms (23.23%), Space: 51.3 MB (31.08%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -4,15 +4,14 @@
  */
 var jump = function(nums) {
     const dp = Array(nums.length).fill(Infinity);
-    dp[0] = 1;
+    dp[0] = 0;
 
     for(let i = 0; i < nums.length; i++) {
-        let maxStep = nums[i];
-        for(let j = 1; j <= maxStep; j++) {
-            if(i + j >= nums.length) break;
-            dp[i + j] = Math.min(dp[i + j], dp[i] + 1);
+        for(let j = 1; j <= nums[i]; j++) {
+            const ni = i + j;
+            if(ni < nums.length) dp[ni] = Math.min(dp[ni], dp[i] + 1);
         }
     }
 
-    return dp.at(-1) - 1;
+    return dp.at(-1);
 };
