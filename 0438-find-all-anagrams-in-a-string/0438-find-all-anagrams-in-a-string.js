e94dd026324cd4decb36600b1d63c4b4729d487e
Jeong seulho <wjdtmfgh@gmail.com>
2024-10-28 16:45:18 +0900

    Time: 23 ms (54.33%), Space: 54.1 MB (76.51%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
@@ -17,22 +17,22 @@ const findAnagrams = (s, p) => {
     
     let left = 0;
     let right = 0; 
-    let needCharLen = p.length;
+    let needCharLen = 0;
     
     while (right < s.length) {
         const curChar = s[right];
         if (curChar in needCharCnt) {
-            if (needCharCnt[curChar] > 0) needCharLen--;
+            if (needCharCnt[curChar] > 0) needCharLen++;
             needCharCnt[curChar]--;
         }
         right++;
         
-        if (needCharLen === 0) ans.push(left);
+        if (needCharLen === p.length) ans.push(left);
         
         if (right - left === p.length) {
             const startChar = s[left];
             if (startChar in needCharCnt) {
-                if (needCharCnt[startChar] >= 0) needCharLen++;
+                if (needCharCnt[startChar] >= 0) needCharLen--;
                 needCharCnt[startChar]++;
             }
             left++;
