62f7cc02a6c7e81b8cb758cb12eccc005ede2538
Jeong seulho <wjdtmfgh@gmail.com>
2024-10-28 09:29:07 +0900

    Time: 25 ms (89.63%), Space: 63.4 MB (42.24%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const ans = {};
+
+    for(const s of strs) {
+        const key = s.split('').sort().join('');
+        if(!ans[key]) ans[key] = [];
+        ans[key].push(s);
+    }
+
+    return Object.values(ans);
+};
