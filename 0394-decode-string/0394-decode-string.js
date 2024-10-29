2802a2a7328b01a412918f0b49ef46f057de69e0
Jeong seulho <wjdtmfgh@gmail.com>
2024-10-29 09:48:27 +0900

    Time: 0 ms (100%), Space: 48.7 MB (71.94%) - LeetHub

--- 0394-decode-string/0394-decode-string.js
+++ 0394-decode-string/0394-decode-string.js
@@ -2,25 +2,22 @@
  * @param {string} s
  * @return {string}
  */
-var decodeString = function (s) {
+var decodeString = function(s) {
     const stack = [];
 
-    for (const char of s) {
-        if (char === "]") {
+    for(const char of s) {
+        if(char === ']') {
             let str = '';
-            let num = '';
-
-            while (stack.length && stack.at(-1) !== '[') str = stack.pop() + str;
-            
+            let times = '';
+            while(stack.at(-1) !== '[') str = stack.pop() + str;
             stack.pop();
-
-            while (stack.length && !isNaN(stack.at(-1))) num = stack.pop() + num;
-
-            stack.push(str.repeat(Number(num)));
+            while(stack.length && !isNaN(stack.at(-1))) times = stack.pop() + times;
+            const completeStr = str.repeat(Number(times));
+            stack.push(completeStr);
         } else {
             stack.push(char);
         }
     }
 
-    return stack.join("");
+    return stack.join('');
 };
