4112bc2d0e07720acc6cf0b27c2e49b01afeee7b
Jeong seulho <wjdtmfgh@gmail.com>
2024-10-29 17:50:58 +0900

    Time: 39 ms (7.14%), Space: 61.4 MB (5.63%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    intervals.sort((a, b) => {
+        return a[0] - b[0];
+    });
+
+    console.log(intervals);
+
+    const stack = [intervals[0]];
+
+    for(let i = 1; i < intervals.length; i++) {
+        const [prevStart, prevEnd] = stack.at(-1);
+        const [start, end] = intervals[i];
+        if(prevEnd >= start) {
+            stack.pop();
+            stack.push([prevStart, Math.max(end, prevEnd)]);
+        }
+        else stack.push([start, end]);
+    }
+
+    return stack;
+};
