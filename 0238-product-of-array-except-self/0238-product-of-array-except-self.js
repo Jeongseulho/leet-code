eb6d0d8f0114becc82b5d35c6e4af0e67b8bf02b
Jeong seulho <wjdtmfgh@gmail.com>
2024-10-30 16:50:42 +0900

    Time: 4 ms (87.92%), Space: 62.6 MB (77.77%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,25 +3,16 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    const dp1 = Array(nums.length).fill(0);
-    dp1[0] = nums[0];
-
-    const dp2 = Array(nums.length).fill(0);
-    dp2[nums.length - 1] = nums[nums.length - 1];
-
+    const ans = Array(nums.length).fill(1);
+    
     for(let i = 1; i < nums.length; i++) {
-        dp1[i] = dp1[i - 1] * nums[i];
+        ans[i] = nums[i - 1] * ans[i - 1];
     }
 
-    for(let i = nums.length - 2; i > -1; i--) {
-        dp2[i] = dp2[i + 1] * nums[i];
-    }
-
-    const ans = Array(nums.length).fill(0);
-    ans[0] = dp2[1];
-    ans[nums.length - 1] = dp1[nums.length - 2]
-    for(let i = 1; i < nums.length - 1; i++) {
-        ans[i] = dp1[i - 1] * dp2[i + 1];
+    let rightAcc = 1;
+    for(let j = nums.length - 2; j > -1; j--) {
+        rightAcc *= nums[j + 1];
+        ans[j] *= rightAcc;
     }
 
     return ans;
