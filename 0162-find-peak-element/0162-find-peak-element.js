971f1679a8e2198f2122ec4c7e23f2c97342e95a
Jeong seulho <wjdtmfgh@gmail.com>
2024-11-01 10:37:29 +0900

    Time: 0 ms (100%), Space: 49.1 MB (31.83%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -3,16 +3,14 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    let low = 0, high = nums.length - 1;
+    let low = 0;
+    let high = nums.length - 1;
+
     while(low <= high) {
         const mid = Math.floor((low + high) / 2);
-        
-        if (mid + 1 < nums.length && nums[mid] < nums[mid + 1]) {
-            low = mid + 1; // 피크는 오른쪽에 있음
-        } else {
-            high = mid - 1; // 피크는 왼쪽에 있음
-        }
-    }
+        if(mid < nums.length - 1 && nums[mid] < nums[mid + 1]) low = mid + 1;
+        else high = mid - 1;
+    }                   
 
-    return low; // 피크 요소를 가리킴
+    return low;
 };
