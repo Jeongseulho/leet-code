3039bc9a099d712e9d31d4f61b17b843d1a0f796
Jeong seulho <wjdtmfgh@gmail.com>
2024-11-05 14:59:28 +0900

    Time: 57 ms (24.09%), Space: 64 MB (96.53%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -6,18 +6,17 @@ var threeSum = function(nums) {
     nums.sort((a, b) => a - b);
     const ans = [];
     for(let i = 0; i < nums.length - 2; i++) {
-        if(i > 0 && nums[i - 1] === nums[i]) continue;
-        let left = i + 1;
-        let right = nums.length - 1;
+        if(i > 0 && nums[i] === nums[i - 1]) continue;
+        left = i + 1;
+        right = nums.length - 1;
         while(left < right) {
-            const sum = nums[i] + nums[left] + nums[right];
-            if(sum < 0) left += 1;
-            else if(sum > 0) right -= 1;
-            else {
+            if(nums[i] + nums[left] + nums[right] === 0) {
                 ans.push([nums[i], nums[left], nums[right]]);
                 left += 1;
-                while(nums[left] === nums[left - 1] && left < right) left += 1;
+                while(left < right && nums[left] === nums[left - 1]) left += 1;
             }
+            else if(nums[i] + nums[left] + nums[right] < 0) left += 1;
+            else right -= 1;
         }
     }
 
