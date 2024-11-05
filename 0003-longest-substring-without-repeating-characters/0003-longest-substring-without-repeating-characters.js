9ec5ce109ed96cc4e8aca917a3cd7b54271e5c8a
Jeong seulho <wjdtmfgh@gmail.com>
2024-11-05 16:18:43 +0900

    Time: 9 ms (75.94%), Space: 54 MB (64.34%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,20 +3,17 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    if(!s) return 0;
     let left = 0;
-    let right = 0;
-    let maxLen = 1;
+    let maxLen = 0;
+    const curStr = new Set();
 
-    while(left < s.length && right < s.length + 1) {
-        right += 1;
-        if((right - left) === new Set(s.substring(left, right)).size) {
-            maxLen = Math.max(maxLen, right - left);
-        } else {
-            while((right - left) !== new Set(s.substring(left, right)).size) {
-                left += 1;
-            }
+    for(let right = 0; right < s.length; right++) {
+        while(curStr.has(s[right])) {
+            curStr.delete(s[left]);
+            left += 1;
         }
+        curStr.add(s[right]);
+        maxLen = Math.max(maxLen, right - left + 1);
     }
 
     return maxLen;
