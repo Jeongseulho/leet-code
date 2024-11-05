123096ff259d394e697ea644620ada1a10b84dcb
Jeong seulho <wjdtmfgh@gmail.com>
2024-11-05 13:28:54 +0900

    Time: 1 ms (23.38%), Space: 49.1 MB (29.28%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    for(let i = 0; i < haystack.length - needle.length + 1; i++) {
+        if(needle === haystack.substring(i, i + needle.length)) return i;
+    }
+
+    return -1;
+};
