4f8e495be0c43006df096be997d080be95bffc94
Jeong seulho <wjdtmfgh@gmail.com>
2024-11-05 12:44:16 +0900

    Time: 22 ms (32.07%), Space: 58.3 MB (19.13%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.js
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -4,31 +4,19 @@
  * @return {string}
  */
 var convert = function(s, numRows) {
-    if(numRows === 1) return s;
-    const arr = Array.from({ length : numRows }, () => Array(1000).fill(''));
+    if (numRows === 1 || numRows >= s.length) return s;
+
+    const arr = Array. from({ length : numRows }, () => []);
+    
     let idx = 0;
-    let i = 0;
-    let j = 0;
-    while(idx < s.length) {
-
-        while(idx < s.length && i < numRows) {
-            arr[i][j] = s[idx];
-            idx += 1;
-            i += 1;
-        }
-        i -= 2;
-        j += 1;
+    let dir = 1;
+    for(const char of s) {
+        arr[idx].push(char);
+        if(idx === 0) dir = 1;
+        else if(idx === numRows - 1) dir = -1;
         
-        while(idx < s.length && i > -1 && j < 1000) {
-            arr[i][j] = s[idx];
-            idx += 1;
-            i -= 1;
-            j += 1;
-        }
-        i += 2;
-        j -= 1;
-
+        idx += dir;
     }
 
-    return arr.reduce((acc, cur) => acc + cur.join(''), '')
+    return arr.map((strs) => strs.join('')).join('');
 };
