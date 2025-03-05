0eb0582ba21cd92b742481558a14ea6a53eb0f84
Jeong seulho <wjdtmfgh@gmail.com>
2025-03-05 21:42:01 +0900

    Time: 44 ms (98.52%), Space: 56.6 MB (20.11%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -2,15 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-removeDuplicates = function(nums) {
-    let k = 0;
-
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== nums[i + 2]) {
-            nums[k] = nums[i];
-            k++;
+var removeDuplicates = function(nums) {
+    let i = 1;
+    let cnt = 1;
+    for(let j = 1; j < nums.length; j++) {
+        if(nums[i - 1] === nums[j]) {
+            if(cnt === 1) {
+                cnt += 1;
+                nums[i] = nums[j];
+                i += 1;
+            }
+        } else {
+            nums[i] = nums[j];
+            cnt = 1;
+            i += 1;
         }
     }
 
-    return k;    
+    return i;
 };
