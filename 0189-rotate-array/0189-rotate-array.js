5c6a7bb0e60031787b93423469d2b0e02548dc8d
Jeong seulho <wjdtmfgh@gmail.com>
2025-03-07 21:28:44 +0900

    Time: 1 ms (84.49%), Space: 63.8 MB (23.18%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -4,16 +4,10 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    k %= nums.length;
-    const reverse = (left, right) => {
-        while(left < right) {
-            [nums[left], nums[right]] = [nums[right], nums[left]];
-            left++;
-            right--;
-        }
+    const rotateCnt = k % nums.length;
+    const origin = [...nums];
+    for(let i = 0; i < nums.length; i++) {
+        const changeIdx = i - rotateCnt < 0 ? nums.length - rotateCnt + i : i - rotateCnt;
+        nums[i] = origin[changeIdx];
     }
-
-    reverse(0, nums.length - 1);
-    reverse(0, k - 1);
-    reverse(k, nums.length - 1);
 };
