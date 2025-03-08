92f85488539c8332c34edaa3fdceadda6733f04d
Jeong seulho <wjdtmfgh@gmail.com>
2025-03-08 14:17:24 +0900

    Time: 4 ms (36.6%), Space: 65.8 MB (7.26%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,12 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let buyPrice = prices[0];
+    const stack = [prices[0]];
     let maxProfit = 0;
-
-    for(const price of prices) {
-        maxProfit = Math.max(maxProfit, price - buyPrice);
-        if(price < buyPrice) buyPrice = price;
+    for(let i = 1; i < prices.length; i++) {
+        const price = prices[i];
+        const minPrice = stack[stack.length - 1];
+        if(minPrice > price) stack.push(price);
+        else maxProfit = Math.max(maxProfit, price - minPrice);
     }
 
     return maxProfit;
