c90e35bdb6f307563a6b58d272321ad4d7860414
Jeong seulho <wjdtmfgh@gmail.com>
2025-03-15 00:08:26 +0900

    Time: 12 ms (35.73%), Space: 70.2 MB (27.44%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -4,15 +4,15 @@
  */
 var productExceptSelf = function(nums) {
     const ans = Array(nums.length).fill(1);
-    
+
     for(let i = 1; i < nums.length; i++) {
         ans[i] = nums[i - 1] * ans[i - 1];
     }
 
-    let rightAcc = 1;
-    for(let j = nums.length - 2; j > -1; j--) {
-        rightAcc *= nums[j + 1];
-        ans[j] *= rightAcc;
+    let acc = 1;
+    for(let i = nums.length - 2; i > -1; i--) {
+        acc *= nums[i + 1];
+        ans[i] *= acc
     }
 
     return ans;
