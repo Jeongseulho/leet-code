a25c4ca8d8776ec3018f5d6d1118a49ddc2251ac
Jeong seulho <wjdtmfgh@gmail.com>
2025-03-26 13:37:43 +0900

    Time: 11 ms (27.73%), Space: 60.6 MB (45.09%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.js
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -3,15 +3,16 @@
  * @return {string}
  */
 var intToRoman = function(num) {
-    const val = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
-    const rom = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+    const val = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+    const rom = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+    let ans = '';
     
-    let ans = ""
-    for (let i = 0; num > 0; i++)
-        while (num >= val[i]) {
+    for(let i = 0; i < val.length; i++) {
+        while(num >= val[i]) {
             ans += rom[i];
             num -= val[i];
         }
+    }
 
-    return ans
+    return ans;
 };
