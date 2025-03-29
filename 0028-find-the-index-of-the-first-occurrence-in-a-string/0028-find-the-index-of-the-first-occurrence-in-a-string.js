d68d8226816f6e6fb01a4cde9a7925c9fb44e04f
Jeong seulho <wjdtmfgh@gmail.com>
2025-03-29 18:46:21 +0900

    Time: 0 ms (100%), Space: 53.9 MB (25.24%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,8 +4,13 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    for(let i = 0; i < haystack.length - needle.length + 1; i++) {
-        if(needle === haystack.substring(i, i + needle.length)) return i;
+    for(let s = 0; s <= haystack.length - needle.length; s++) {
+        let cnt = 0;
+        for(let i = s; i < s + needle.length; i++) {
+            if(haystack[i] === needle[i - s]) cnt += 1;
+            else break;
+        }
+        if(cnt === needle.length) return s;
     }
 
     return -1;
