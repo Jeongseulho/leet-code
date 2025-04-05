493da58bace025c25a9137dbdaf5049ecbeb4b25
Jeong seulho <wjdtmfgh@gmail.com>
2025-04-05 18:58:49 +0900

    Time: 4 ms (37.36%), Space: 59.5 MB (13.21%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -4,28 +4,21 @@
  * @return {number}
  */
 var minSubArrayLen = function(target, nums) {
-    if(nums.reduce((acc, cur) => acc + cur, 0) < target) return 0;
-    let left = 0;
-    let right = 1;
-    let sum = nums[0];
-    let minLen = nums.length;
-
-    while(left < nums.length && right < nums.length + 1) {
-        if(sum < target) {
-            sum += nums[right];
-            right += 1;
-        } else if(sum > target) {
-            minLen = Math.min(minLen, right - left);
-            sum -= nums[left];
-            left += 1;
-        } else {
-            minLen = Math.min(minLen, right - left);
-            sum += nums[right];
-            sum -= nums[left];
-            right += 1;
-            left += 1;
+    let minLen = nums.length + 1;
+    let s = 0;
+    let e = 0;
+    let sum = 0;
+    while(e !== nums.length && s !== nums.length) {
+        while(e !== nums.length && sum < target) {
+            sum += nums[e];
+            e++;
         }
+        while(s !== nums.length && sum >= target) {
+            sum -= nums[s];
+            s++;
+        }
+        minLen = Math.min(minLen, e - s + 1);
     }
 
-    return minLen;
+    return minLen === nums.length + 1 ? 0 : minLen;
 };
