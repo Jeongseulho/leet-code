7b9da2a0673d639a7066823b6ac56fad813332fe
Jeong seulho <wjdtmfgh@gmail.com>
2025-04-24 23:32:33 +0900

    Time: 0 ms (100%), Space: 53.9 MB (20.39%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.js
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -5,30 +5,23 @@
 var spiralOrder = function(matrix) {
     const m = matrix.length;
     const n = matrix[0].length;
-    const left = [0, -1];
-    const right = [0, 1];
-    const down = [1, 0];
-    const up = [-1, 0];
-    const dir = [right, down, left, up];
-    const visited = Array.from({length:m}, () => Array(n).fill(false));
-    let visitCnt = 0;
+
+    const ans = [];
+    const di = [0, 1, 0, -1];
+    const dj = [1, 0, -1, 0];
+    let dir = 0;
     let ci = 0;
     let cj = 0;
-    let dirCnt = 0;
-    const output = [];
 
-    while(visitCnt < m * n) {
-        output.push(matrix[ci][cj]);
-        visited[ci][cj] = true;
-        const ni = ci + dir[dirCnt][0];
-        const nj = cj + dir[dirCnt][1];
-        if(ni < 0 || ni >= m || nj < 0 || nj >= n || visited[ni][nj]) {
-            dirCnt = (dirCnt + 1) % 4;
-        }
-        ci += dir[dirCnt][0];
-        cj += dir[dirCnt][1];
-        visitCnt += 1;
+    while(ans.length < (m * n)) {
+        ans.push(matrix[ci][cj]);
+        matrix[ci][cj] = '';
+        const ni = ci + di[dir];
+        const nj = cj + dj[dir];
+        if(ni === -1 || nj === -1 || ni === m || nj === n || matrix[ni][nj] === '') dir = (dir + 1) % 4;
+        ci += di[dir];
+        cj += dj[dir];
     }
 
-    return output;
+    return ans;
 };
