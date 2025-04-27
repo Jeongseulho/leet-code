769995e2a32e292cdc5a6cd02a7c658fd7d1f6ef
Jeong seulho <wjdtmfgh@gmail.com>
2025-04-27 21:59:05 +0900

    Time: 0 ms (100%), Space: 54.6 MB (29.5%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -4,19 +4,14 @@
  */
 var rotate = function(matrix) {
     const n = matrix.length;
-    let top = 0;
-    let bottom = n - 1;
-
-    while (bottom > top) {
-        for (let j = 0; j < n; j++) {
-            [matrix[top][j], matrix[bottom][j]] = [matrix[bottom][j], matrix[top][j]];
+    for(let i = 0; i < Math.floor(n / 2); i++) {
+        for(let j = 0; j < n; j++) {
+            [matrix[i][j], matrix[n - 1 - i][j]] = [matrix[n - 1 - i][j], matrix[i][j]];
         }
-        top++;
-        bottom--;
     }
 
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < i; j++) { 
+    for(let i = 0; i < n; i++) {
+        for(let j = i + 1; j < n; j++) {
             [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
         }
     }
