ea75e9f1a359f26b2883ff8045039a408a780c4b
Jeong seulho <wjdtmfgh@gmail.com>
2025-05-12 23:57:52 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,13 +3,15 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    let maxLen = 0;
     const numSet = new Set(nums);
+    let maxLen = 0;
+
     for(const num of nums) {
         if(!numSet.has(num - 1)) {
             let len = 1;
-            while(numSet.has(num + len)) len += 1;
-            maxLen = Math.max(len, maxLen);
+            let start = num;
+            while(numSet.has(start + len)) len += 1;
+            maxLen = Math.max(maxLen, len);
         }
     }
 
