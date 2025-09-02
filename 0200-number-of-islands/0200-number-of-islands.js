7dafe89ae6cbafa9c1d1c383b10e7fd77ab43f28
Jeong seulho <wjdtmfgh@gmail.com>
2025-09-02 12:38:50 +0900

    Time: 62 ms (57.7%), Space: 62.3 MB (44.4%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,36 +3,32 @@
  * @return {number}
  */
 var numIslands = function(grid) {
+    let cnt = 0;
     const m = grid.length;
     const n = grid[0].length;
 
     const di = [-1, 1, 0, 0];
     const dj = [0, 0, -1, 1];
-
     const dfs = (si, sj) => {
+        cnt += 1;
         const needVisit = [[si, sj]];
-        
+        grid[si][sj] = '0';
+
         while(needVisit.length) {
             const [ci, cj] = needVisit.pop();
             for(let k = 0; k < 4; k++) {
                 const ni = ci + di[k];
                 const nj = cj + dj[k];
-                if(ni > -1 && ni < m && nj > -1 && nj < n && grid[ni][nj] === '1') {
-                    needVisit.push([ni, nj]);
-                    grid[ni][nj] = '0';
-                }
+                if(ni < 0 || ni >= m || nj < 0 || nj >= n || grid[ni][nj] === '0') continue;
+                needVisit.push([ni, nj]);
+                grid[ni][nj] = '0';
             }
         }
     }
 
-    let cnt = 0;
-
-    for(let si = 0; si < m; si++) {
-        for(let sj = 0; sj < n; sj++) {
-            if(grid[si][sj] === '1') {
-                dfs(si, sj)
-                cnt += 1;
-            }
+    for(let i = 0; i < m; i++) {
+        for(let j = 0; j < n; j++) {
+            if(grid[i][j] === '1') dfs(i, j);
         }
     }
 
