260e8ee6ab9793da3f189d4b4d898ce021e13058
Jeong seulho <wjdtmfgh@gmail.com>
2025-09-07 17:55:15 +0900

    Time: 0 ms (100%), Space: 54.3 MB (10.11%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,10 +3,11 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    const dp = Array(n + 1).fill(0);
+    const dp = new Array(n + 1).fill(0);
+    dp[0] = 1;
     dp[1] = 1;
     dp[2] = 2;
-    for(let i = 3; i <= n; i++) {
+    for (let i = 3; i < n + 1; i++) {
         dp[i] = dp[i - 1] + dp[i - 2];
     }
 
