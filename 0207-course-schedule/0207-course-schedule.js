091b6d124296d753121c90c232b1a5deb431a066
Jeong seulho <wjdtmfgh@gmail.com>
2025-09-09 10:33:35 +0900

    Time: 6 ms (96.95%), Space: 60.6 MB (90.76%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -4,31 +4,33 @@
  * @return {boolean}
  */
 var canFinish = function(numCourses, prerequisites) {
-    const adjList = Array.from({ length: numCourses }, () => []);
+    const adjList = Array.from({length: numCourses }, () => []);
     prerequisites.forEach(([course, pre]) => {
         adjList[pre].push(course);
-    });
+    })
 
-    const visited = new Array(numCourses).fill(false);
-    const onPath = new Array(numCourses).fill(false);
+    const visited = new Set();
+    const path = new Set();
 
     const hasCycle = (course) => {
-        if (onPath[course]) return true;
-        if (visited[course]) return false; 
+        if (path.has(course)) return true;
+        if (visited.has(course)) return false;
 
-        visited[course] = true;
-        onPath[course] = true;
+        visited.add(course);
+        path.add(course);
 
         for (const nextCourse of adjList[course]) {
             if (hasCycle(nextCourse)) return true;
         }
 
-        onPath[course] = false;
+        path.delete(course);
         return false;
     }
 
     for (let i = 0; i < numCourses; i++) {
-        if (!visited[i] && hasCycle(i)) return false;
+        if (!visited.has(i)) {
+            if (hasCycle(i)) return false;
+        }
     }
 
     return true;
