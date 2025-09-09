97d5b97edd3085343d8e3b1995c5ebb64c5dbf60
Jeong seulho <wjdtmfgh@gmail.com>
2025-09-09 15:05:00 +0900

    Time: 7 ms (88.27%), Space: 60.8 MB (80.84%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.js
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -4,37 +4,34 @@
  * @return {number[]}
  */
 var findOrder = function(numCourses, prerequisites) {
-    const adjList = Array.from({ length : numCourses }, () => []);
-    const revAdjList = Array.from({ length : numCourses }, () => []);
+    const adjList = Array.from({ length: numCourses }, () => []);
+    const inDegree = new Array(numCourses).fill(0);
 
-    prerequisites.forEach(([v1, v2]) => {
-        adjList[v1].push(v2);
-        revAdjList[v2].push(v1);
-    });
-
-    let needVisit = [];
-    for(let courseNum = 0; courseNum < numCourses; courseNum++) {
-        if(!adjList[courseNum].length) needVisit.push(courseNum);
+    for (const [course, pre] of prerequisites) {
+        adjList[pre].push(course);
+        inDegree[course]++;
     }
 
-    const visited = [];
-    while(needVisit.length) {
-        let visitableCourse;
-        for(const course of needVisit) {
-            const preCourses = adjList[course];
-            if(!preCourses.length || preCourses.every((preCourse) => visited.includes(preCourse))) {
-                visitableCourse = course;
-                needVisit = needVisit.filter((course) => course !== visitableCourse);
-                break;
+    const queue = [];
+    for (let i = 0; i < numCourses; i++) {
+        if (inDegree[i] === 0) {
+            queue.push(i);
+        }
+    }
+
+    const result = [];
+    
+    while (queue.length > 0) {
+        const course = queue.shift();
+        result.push(course);
+
+        for (const nextCourse of adjList[course]) {
+            inDegree[nextCourse]--;
+            if (inDegree[nextCourse] === 0) {
+                queue.push(nextCourse);
             }
         }
-        if(visitableCourse === undefined) return [];
-
-        if(visited.includes(visitableCourse)) continue;
-
-        visited.push(visitableCourse);
-        revAdjList[visitableCourse].forEach((next) => needVisit.push(next));
     }
-    
-    return visited.length === numCourses ? visited : [];
+
+    return result.length === numCourses ? result : [];
 };
