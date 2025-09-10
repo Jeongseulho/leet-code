a80ab75ed6d453ae39f84f2f838fed95ce908d20
Jeong seulho <wjdtmfgh@gmail.com>
2025-09-10 13:51:11 +0900

    Time: 8 ms (84.54%), Space: 56.2 MB (96.62%) - LeetHub

--- 0945-snakes-and-ladders/0945-snakes-and-ladders.js
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.js
@@ -4,34 +4,44 @@
  */
 var snakesAndLadders = function(board) {
     const n = board.length;
-    
-    const transNumToPos = (num) => {
-        const row = Math.floor((num - 1) / n);
-        const col = (num - 1) % n;
-        const actualRow = n - 1 - row;
-        const actualCol = row % 2 === 0 ? col : n - 1 - col;
-        return [actualRow, actualCol];
-    };
 
-    const needVisit = [[1, 0]];
+const convertPosToIdx = (pos) => {
+    // 1번 칸부터 시작하므로 pos - 1
+    // 보드 아래에서부터 0번 줄, 1번 줄...
+    const rowFromBottom = Math.floor((pos - 1) / n);
+    // 해당 줄의 왼쪽에서부터 몇 번째 칸인지
+    const colFromLeft = (pos - 1) % n;
+
+    // 실제 배열 인덱스 i (위에서부터)
+    const i = n - 1 - rowFromBottom;
+    
+    let j;
+    // 아래에서부터 센 줄(rowFromBottom)이 짝수 줄이면 (0, 2, 4...) 왼쪽 -> 오른쪽
+    if (rowFromBottom % 2 === 0) {
+        j = colFromLeft;
+    } else { // 홀수 줄이면 (1, 3, 5...) 오른쪽 -> 왼쪽
+        j = n - 1 - colFromLeft;
+    }
+
+    return {i, j};
+}
+
+    const queue = [[1, 0]];
     const visited = new Set();
     visited.add(1);
+    
+    while (queue.length) {
+        const [pos, cnt] = queue.shift();
+        for(let dice = 1; dice < 7; dice++) {
+            let next = dice + pos;
+            const {i, j} = convertPosToIdx(next);
+            if(board[i][j] !== -1) next = board[i][j];
+            if(next >= n * n) return cnt + 1;
+            
+            if(visited.has(next)) continue;
 
-    while (needVisit.length) {
-        const [cur, cnt] = needVisit.shift();
-
-        for (let dice = 1; dice <= 6; dice++) {
-            let next = cur + dice;
-
-            const [ni, nj] = transNumToPos(next);
-            if (board[ni][nj] !== -1) next = board[ni][nj];
-
-            if (next >= n * n) return cnt + 1;
-
-            if (!visited.has(next)) {
-                visited.add(next);
-                needVisit.push([next, cnt + 1]);
-            }
+            queue.push([next, cnt + 1]);
+            visited.add(next);
         }
     }
 
