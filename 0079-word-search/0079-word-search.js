be416bae75b29d0765ac3bba82d6ea5a86aa95db
Jeong seulho <wjdtmfgh@gmail.com>
2025-10-02 14:13:39 +0900

    Time: 346 ms (44.61%), Space: 55.7 MB (49.97%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -4,32 +4,36 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
-    const di = [0, 0, -1, 1];
-    const dj = [-1, 1, 0, 0];
     const m = board.length;
     const n = board[0].length;
 
-    const dfs = (idx, ci, cj) => {
-        if (idx === word.length) return true;
-        if (ci < 0 || ci >= m || cj < 0 || cj >= n || board[ci][cj] !== word[idx]) return false;
+    const di = [-1, 1, 0, 0];
+    const dj = [0, 0, -1, 1];
 
-        const temp = board[ci][cj];
-        board[ci][cj] = '#';
+    const dfs = (i, j, idx) => {
+        if(idx === word.length) return true;
+        if(i < 0 || i >= m || j < 0 || j >= n || board[i][j] !== word[idx]) return false;
 
-        for (let k = 0; k < 4; k++) {
-            const ni = ci + di[k];
-            const nj = cj + dj[k];
-            if (dfs(idx + 1, ni, nj)) return true;
+        const originLetter = board[i][j];
+        board[i][j] = 'visited';
+
+        for(let k = 0; k < 4; k ++) {
+            const ni = i + di[k];
+            const nj = j + dj[k];
+            if(dfs(ni, nj, idx + 1)) return true;
         }
 
-        board[ci][cj] = temp;
-
+        board[i][j] = originLetter;
         return false;
-    };
+    }
 
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (dfs(0, i, j)) return true;
+    for (let si = 0; si < m; si++) {
+        for (let sj = 0; sj < n; sj++) {
+            if (board[si][sj] === word[0]) {
+                 if (dfs(si, sj, 0)) {
+                    return true;
+                }
+            }
         }
     }
 
