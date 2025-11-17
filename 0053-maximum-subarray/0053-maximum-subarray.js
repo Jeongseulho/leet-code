c5e9ff2c111370133fbbf665b7706b966cc49c6e
Jeong seulho <wjdtmfgh@gmail.com>
2025-11-17 16:16:50 +0900

    Time: 4 ms (21.07%), Space: 63.6 MB (72.9%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,14 +3,8 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let maxSum = nums[0];
-    let sum = 0;
-
-    for (let num of nums) {
-        if (sum < 0) sum = 0;
-        sum += num;
-        maxSum = Math.max(maxSum, sum);
-    }
-    
-    return maxSum;    
+    for (let i = 1; i < nums.length; i++) {
+        nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+    };
+    return Math.max(...nums);
 };
