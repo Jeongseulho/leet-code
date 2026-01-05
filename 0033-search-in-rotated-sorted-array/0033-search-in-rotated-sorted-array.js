fc7e73c9cf4e1be27871f512a94f46828200981a
Jeong seulho <wjdtmfgh@gmail.com>
2026-01-05 19:07:05 +0900

    Time: 0 ms (100%), Space: 54.3 MB (57.73%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -9,7 +9,7 @@ var search = function(nums, target) {
     while(left <= right) {
         const mid = Math.floor((left + right) / 2);
         if(nums[mid] === target) return mid;
-        if(nums[left] < nums[mid]) {
+        if(nums[left] <= nums[mid]) {
             if(nums[left] <= target && target < nums[mid]) right = mid - 1;
             else left = mid + 1;
         } else {
