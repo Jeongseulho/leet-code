5d03a5bdee17a9543668067f4e296ab5400c72dd
Jeong seulho <wjdtmfgh@gmail.com>
2026-01-23 16:31:57 +0900

    Time: 0 ms (100%), Space: 55.9 MB (14.06%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,22 +4,25 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    let low = 0, high = nums.length - 1;
-    while (low <= high) {
-        const mid = Math.floor((low + high) / 2);
-        if(target <= nums[mid]) high = mid - 1;
-        else low = mid + 1;
-    }
-    const s = low < nums.length && nums[low] === target ? low : -1;
 
-    low = 0
-    high = nums.length - 1;
-    while (low <= high) {
-        const mid = Math.floor((low + high) / 2);
-        if(nums[mid] <= target) low = mid + 1;
-        else high = mid - 1;
-    }
-    const e = -1 < high && nums[high] === target ? high : -1;
+    const bs = (findLeft) => {
+        let left = 0;
+        let right = nums.length - 1;
+        let ans = -1;
 
-    return [s, e];
+        while(left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            if(nums[mid] < target) left = mid + 1;
+            if(nums[mid] > target) right = mid - 1;
+            if(nums[mid] === target) {
+                ans = mid;
+                if(findLeft) right = mid - 1;
+                else left = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    return [bs(true), bs(false)];
 };
