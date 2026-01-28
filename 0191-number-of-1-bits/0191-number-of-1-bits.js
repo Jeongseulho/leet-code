7615f5707a1af0c37aa9e21cd3f531042b8ffeec
Jeong seulho <wjdtmfgh@gmail.com>
2026-01-28 18:27:13 +0900

    Time: 0 ms (100%), Space: 54.1 MB (87.52%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let ans = 0;
+    for (let i = 0; i < 32; i++) {
+        if (n & 1) ans++;
+        n >>= 1;
+    }
+    return ans;
+};
