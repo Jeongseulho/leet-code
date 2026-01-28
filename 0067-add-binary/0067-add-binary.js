21566674e00dc602ff7de1139709cdfe636a8008
Jeong seulho <wjdtmfgh@gmail.com>
2026-01-28 15:54:21 +0900

    Time: 1 ms (69.22%), Space: 55.8 MB (41.92%) - LeetHub

--- 0067-add-binary/0067-add-binary.js
+++ 0067-add-binary/0067-add-binary.js
@@ -4,6 +4,23 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-    let sum = BigInt(`0b${a}`) + BigInt(`0b${b}`);
-    return sum.toString(2);
+    let i = a.length - 1;
+    let j = b.length - 1;
+    let carry = 0;
+    let ans = [];
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+        const valA = i >= 0 ? Number(a[i]) : 0;
+        const valB = j >= 0 ? Number(b[j]) : 0;
+        
+        const sum = valA + valB + carry;
+        
+        ans.push(sum % 2);
+        carry = Math.floor(sum / 2);
+        
+        i--;
+        j--;
+    }
+
+    return ans.reverse().join('');
 };
