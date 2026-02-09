efef51942ba45ee89ab7facabefc3438973c1cab
Jeong seulho <wjdtmfgh@gmail.com>
2026-02-09 13:46:06 +0900

    Time: 1 ms (73.5%), Space: 56.3 MB (36.4%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    if(x < 2) return x;
+    let left = 1;
+    let right = Math.floor(x / 2);
+
+    while(left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        const pow = mid * mid;
+        if(pow <= x) left = mid + 1;
+        else right = mid - 1;
+    }
+
+    return right;
+};
